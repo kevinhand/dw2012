bda0e387821c81d9be04113a8fded28894a08956
Xiao Jia <stfairy@gmail.com>
2012-09-23 01:10:10 +0800

    finish CiteSeerParser

--- app/parsers/CiteSeerParser.php
+++ app/parsers/CiteSeerParser.php
@@ -3,14 +3,36 @@ class CiteSeerParser
 {
   public function accepts($url)
   {
-    // TODO
-    return FALSE;
+    return preg_match('@^(?:https?://)?citeseerx.ist.psu.edu/viewdoc/summary\?doi=(?P<doi>[^/]+)@', $url, $this->matches);
   }
   
   public function parse($url)
   {
-    // TODO
-    return array();
+    $doi = $this->matches['doi'];
+    $html = file_get_contents("http://citeseerx.ist.psu.edu/viewdoc/summary?doi={$doi}");
+    if (preg_match('@(?:<meta name="description" content=")(?P<description>.+?)"\s*/>@', $html, $matches)) {
+      $this->description = $matches['description'];
+    }
+    if (preg_match('@(?:<meta name="citation_title" content=")(?P<title>.+?)"\s*/>@', $html, $matches)) {
+      $this->title = $matches['title'];
+    }
+    if (preg_match('@(?:<meta name="citation_authors" content=")(?P<authors>.+?)"\s*/>@', $html, $matches)) {
+      $this->authors = $matches['authors'];
+    }
+    if (preg_match('@(?:<meta name="citation_year" content=")(?P<year>.+?)"\s*/>@', $html, $matches)) {
+      $this->year = $matches['year'];
+    }
+    if (preg_match('@(?:<meta name="citation_conference" content=")(?P<conference>.+?)"\s*/>@', $html, $matches)) {
+      $this->conference = $matches['conference'];
+    }
+    return array(
+      'type' => 'paper',
+      'title' => $this->title,
+      'authors' => $this->authors,
+      'year' => $this->year,
+      'conference' => $this->conference,
+      'description' => $this->description
+    );
   }
 }
 
